baf70e0e4711727ea039c4490c18e1715174152e
Debola Adedapo <niikee80@hotmail.com>
2024-09-16 19:43:24 +0100

    landing page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,10 @@ import { EndpointService } from './services/endpoint.service';
 import { HttpRequestInterceptor } from './interceptor/http-request.interceptor';
 import { HttpResponseInterceptor } from './interceptor/http-response.interceptor';
 import { SettingsService } from './services/settings.service';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CardComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [
     EndpointService,
@@ -41,39 +42,6 @@ import { SettingsService } from './services/settings.service';
 export class AppModule {}
 
 ///Upload config file
-
 export function initAppFactory(init: SettingsService, http: HttpClient) {
   return () => init.initAppConfig();
 }
-// export function initAppFactory(): Promise<any> {
-//   const settingService = inject(SettingsService);
-//   const http = inject(HttpClient);
-
-//   return new Promise((resolve) => {
-//     if (environment.production) {
-//       http
-//         .get('./config.json')
-//         .pipe(
-//           tap((data: any) => {
-//             settingService.baseUrl = data.baseUrl;
-//             settingService.apiHash = data.HASH;
-//             settingService.apiKey = data.API_KEY;
-//             settingService.apiTS = data.TS;
-//             resolve(true);
-//           }),
-//           catchError((error) => {
-//             resolve(true);
-//             return of(null);
-//           })
-//         )
-//         .subscribe();
-//     } else {
-//       const settings = require('../../config.json');
-//       settingService.baseUrl = settings.BASE_URL;
-//       settingService.apiHash = settings.HASH;
-//       settingService.apiKey = settings.API_KEY;
-//       settingService.apiTS = settings.TS;
-//       resolve(true);
-//     }
-//   });
-// }
---
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, catchError, of } from 'rxjs';
+import { UnSub } from '../../utils/unsubscribe';
+import { ICharacter } from '../../model/character.td';
+import { EndpointService } from '../../services/endpoint.service';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss',
+})
+export class LandingPageComponent extends UnSub implements OnInit {
+  errorMessageSubject = new BehaviorSubject<string>('');
+  errorMessageAction$ = this.errorMessageSubject.asObservable();
+
+  characterList = new BehaviorSubject<ICharacter[]>([]);
+  characterList$ = this.characterList.asObservable();
+
+  constructor(private endpointService: EndpointService) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.getAllCharacters();
+  }
+
+  getAllCharacters() {
+    this.endpointService.characterList$
+      .pipe(
+        catchError((error) => {
+          this.errorMessageSubject.next(error);
+          return of([]);
+        })
+      )
+      .subscribe({
+        next: (users: ICharacter[]) => {
+          this.characterList.next(users);
+        },
+        error: (error: any) => console.log(error),
+      });
+  }
+}
